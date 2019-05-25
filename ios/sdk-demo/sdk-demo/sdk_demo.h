468658c5c7c2fdd378ac3bcf8e486a953a69dbf3
amano <amanoooooooooooooooo@gmail.com>
2019-05-25 20:31:14 +0800

    Add sdk-demo

---
+++ ios/sdk-demo/sdk-demo/sdk_demo.h
@@ -0,0 +1,19 @@
+//
+//  sdk_demo.h
+//  sdk-demo
+//
+//  Created by hikaru amano on 2019/5/25.
+//  Copyright © 2019 hikaru amano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for sdk_demo.
+FOUNDATION_EXPORT double sdk_demoVersionNumber;
+
+//! Project version string for sdk_demo.
+FOUNDATION_EXPORT const unsigned char sdk_demoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sdk_demo/PublicHeader.h>
+
+
